9d132c60dee632a78b683eff05565175a75e65a1
John Cooper <johnxcooper87@gmail.com>
2019-08-13 11:30:54 +0700

    update

--- src/lib/attributeToProp.js
+++ src/lib/attributeToProp.js
@@ -1,6 +1,6 @@
 export function attributeToProp(attributes, current) {
-  const [name, _value] = current.split('=');
-  let value = _value.substring(1, _value.length - 1);
+  const [name, ..._value] = current.split('=');
+  let value = _value.join('=') |> #.substring(1, #.length - 1);
 
   if (value === 'true') value = true;
   else if (value === 'false') value = false;
